c6996c962f2d209167a33ea1e6977b40939c9c75
Hardik Sarkar <hardiksarkar198@gmail.com>
2023-12-30 21:49:35 +0530

    corrections

--- src/components/Bookstore.js
+++ src/components/Bookstore.js
@@ -18,8 +18,7 @@ export default function Bookstore({morebooks}) {
         <div className='more-books'>
             {
                 morebooks.map((book)=>{
-                  // console.log(book.id);
-                    return <img onClick={()=>onBookClick(book.volumeInfo.infoLink)} className='books' src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} key={book.id}/>
+                    return book.volumeInfo.imageLinks && <img onClick={()=>onBookClick(book.volumeInfo.infoLink)} className='books' src={book.volumeInfo.imageLinks?book.volumeInfo.imageLinks.thumbnail:""} alt={book.volumeInfo.title} key={book.id}/>
                 })
             }
         </div>
